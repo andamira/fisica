cf637c8d8fd382817be23c624b6a1cb95f40e139
joseLuís <joseluis@andamira.net>
2023-03-03 13:45:28 +0100

    add features `std`, `safe`, `nightly`
    - update lib lints and cfg.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
+// fisica
+//
 //! A physics library
-//!
+//
+
+#![warn(clippy::all)]
+#![allow(
+    clippy::float_arithmetic,
+    clippy::implicit_return,
+    clippy::needless_return,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::pattern_type_mismatch
+)]
+//
+#![cfg_attr(feature = "safe", forbid(unsafe_code))]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(feature = "nightly", feature(doc_cfg))]
 
 #[macro_use]
 mod auto_impls;
