b907aa9a78421bfe27407003386fd1a49bfc44d9
joseLuís <joseluis@andamira.net>
2023-08-24 13:24:39 +0200

    update warning allowances

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,7 @@
 //
 
 #![warn(clippy::all)]
-#![allow(
-    clippy::float_arithmetic,
-    clippy::implicit_return,
-    clippy::needless_return,
-    clippy::blanket_clippy_restriction_lints,
-    clippy::pattern_type_mismatch
-)]
+#![cfg_attr(not(feature = "alloc"), allow(unused_macros))]
 //
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "safe", forbid(unsafe_code))]
